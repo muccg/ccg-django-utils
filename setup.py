db724e053a93117a96a53a76d8c9e0d3c55b2015
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-02-03 17:25:08 +0000

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
-import sys, os
 
 setup(name='ccg-django-utils',
-      version='0.3.4',
+      version='0.4.0',
       description='Extra CCG code for Django applications.',
       long_description='Extra code used by the Centre for Comparative Genomics in our Django applications.',
       classifiers=[], # see http://pypi.python.org/pypi?:action=list_classifiers
