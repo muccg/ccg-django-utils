68ead1d0b5300129939e63787a609e0b64e44e84
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-02-04 17:21:17 +0000

    Bump version to 0.4.1, update repo URL

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='ccg-django-utils',
-      version='0.4.0',
+      version='0.4.1',
       description='Extra CCG code for Django applications.',
       long_description='Extra code used by the Centre for Comparative Genomics in our Django applications.',
       classifiers=[], # see http://pypi.python.org/pypi?:action=list_classifiers
       keywords='',
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
-      url='https://bitbucket.org/ccgmurdoch/ccg-django-extras',
+      url='https://github.com/muccg/ccg-django-utils',
       license='GNU General Public License, version 2',
       packages=find_packages(exclude=['examples', 'tests']),
       include_package_data=True,
